86327e46bb8e02dc617f8aa1dc2f60a4875f94ff
newone-07 <135627461+newone-07@users.noreply.github.com>
2023-07-16 11:03:44 +0530

    Add files via upload

--- SimplePatten.java
+++ SimplePatten.java
@@ -1,5 +1,16 @@
 package practice;
-import java.util
+import java.util.Scanner;
 public class SimplePatten {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the number ");
+		int input = scanner.nextInt();
+		for(int i = 1;i<=input;i++) {
+			for(int j =1;j<=i;j++) {
+				System.out.print("*");
+			}	
+			System.out.println("");
+		}
+	}
 
 }
