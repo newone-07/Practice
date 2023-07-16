7036c62b6085c3ff738beaabec8f665aa4ae6fcb
newone-07 <135627461+newone-07@users.noreply.github.com>
2023-07-17 04:37:49 +0530

    SimplePattern3.java

--- SimplePattern3.java
+++ SimplePattern3.java
@@ -2,6 +2,14 @@ package practice;
 import java.util.Scanner;
 public class SimplePattern3 {
 public static void main(String[] args) {
+	/*Enter a character 
+E
+EEEEE
+DDDD
+CCC
+BB
+A
+*/
 	Scanner scanner = new Scanner(System.in);
 	System.out.println("Enter a character ");
 	char input = scanner.next().charAt(0);
